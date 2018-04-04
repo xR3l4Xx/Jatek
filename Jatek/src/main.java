b6e8463804ff980122fb4547f8f008338a190594
Mojo <halloka.mangoka@gmail.com>
2018-04-04 23:55:03 +0200

    Kuka
    Leírás

--- Jatek/src/main.java
+++ Jatek/src/main.java
@@ -1,7 +1,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hasznaljuk amit irtal engin-t, vagy csinaljunk ujat es faszat?:D");
+		System.out.println("Írjunk újat");
 	}
 
 }
